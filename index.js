436ea6943c31c3c938b9296b73d987faf0c5ecce
Jon Nordby <jononor@gmail.com>
2017-02-19 21:31:43 +0100

    Prefer built .js, fallback to .coffee on-demand

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-require('coffee-script/register');
-module.exports = require('./src/index');
+try {
+  module.exports = require('./browser/fbp-spec.js');
+} catch (e) {
+  console.log("fbp-spec: Failed to load built file, loading source. Error:", e);
+  require('coffee-script/register');
+  module.exports = require('./src/index');
+}
